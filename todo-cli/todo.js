a8518b4ec2664cfcd32cbd140d01532d0849f3d4
Darshan T P <darshantp13@gmail.com>
2024-09-25 06:21:49 +0530

    Add pre-commit hook to run tests

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -98,3 +98,5 @@ let itemsDueLater = todos.dueLater();
 let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
 console.log(formattedItemsDueLater);
 console.log("\n\n");
+
+module.exports=todoList;
